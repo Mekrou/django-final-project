960a24454052a5f97615ea50f118bc6f1a409b15
Mekrou <mills.ryan.011@gmail.com>
2023-12-07 14:19:38 -0500

    Added Student model in models.py

--- registration/schedule/models.py
+++ registration/schedule/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES = [
+    ("FR", "Freshman"),
+    ("SO", "Sophomore"),
+    ("JR", "Junior"),
+    ("SR", "Senior"),
+    ("GR", "Graduate"),
+]
+
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    grade_point_avg = models.DecimalField(max_digits=3, decimal_places=2)
+    major = models.CharField(max_length=50)
+    year_in_school = models.CharField(max_length=30, choices=YEAR_IN_SCHOOL_CHOICES)
