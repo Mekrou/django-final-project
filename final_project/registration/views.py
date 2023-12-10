0dc28faa0519f160e092961428fac52b1f80e9bd
Mekrou <mills.ryan.011@gmail.com>
2023-12-10 17:01:02 -0500

    Static files working, and basic registration page layout done

--- final_project/registration/views.py
+++ final_project/registration/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def registration_page(request):
+    input = request.POST.get('battle.net_id')
+    print(input)
+    print(f'type: {type(input)}')
     return render(request, 'registration/registration_page.html')
