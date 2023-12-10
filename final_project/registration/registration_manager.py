6c4db8fe3b469ac5142b0288feb9ddefdb5f7dae
Mekrou <mills.ryan.011@gmail.com>
2023-12-10 18:52:54 -0500

    Registration input validation complete

--- final_project/registration/registration_manager.py
+++ final_project/registration/registration_manager.py
@@ -1,3 +1,5 @@
+from ow2_rank_tracker.models import Player
+
 def contains_symbols(input_string):
     # Check if the string contains symbols
     for char in input_string:
@@ -8,7 +10,9 @@ def contains_symbols(input_string):
 def is_valid_battlenet(input):
     # Does the input have an id? (There is a # in it?)
     result = input.split('#')
-    if len(result) == 1:
+
+    # len(result) should always be 2
+    if not (len(result) == 2):
         # Then we know that there was no '#' found in the input
         return False
     # If there is, lets get the username and id number
@@ -31,3 +35,21 @@ def is_valid_battlenet(input):
     
     # If we passed all checks, it's valid!
     return True
+
+def is_battlenet_in_database(input):
+    username, id = input.split('#')
+
+    try:
+        # search for our player
+        player_object = Player.objects.filter(nickname=username, player_id=id)
+
+        # attempting to access player's nickname
+        # will throw an error if it was not found
+        # which is what we want.
+        player_object.first().nickname
+
+        # no error means it is in DB
+        return True
+    except (AttributeError) as e:
+        #print(f"{input} was not found in the database!")
+        return False
--- final_project/registration/views.py
+++ final_project/registration/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .registration_manager import is_valid_battlenet
+from .registration_manager import is_valid_battlenet, is_battlenet_in_database
+
+from ow2_rank_tracker.models import Player
 # Create your views here.
 def registration_page(request):
     input = request.POST.get('battle.net_id')
@@ -14,5 +16,19 @@ def registration_page(request):
     # as in https://eu.battle.net/support/en/article/26963
     is_valid = is_valid_battlenet(input)
     print(f"Result from is_valid..: {is_valid}")
+    if (not is_valid):
+        return render(request, 'registration/registration_page.html')
 
-    return render(request, 'registration/registration_page.html')
+    # Final case is to check if it is already in our database
+    is_in_db = is_battlenet_in_database(input)
+    print(f"Result from is_in_db..: {is_in_db}")
+    if (not is_in_db):
+        # add to db
+        username, id = input.split('#')
+        new_player = Player(nickname=username, player_id=id)
+        new_player.save()
+        #TODO: SEND SIGNAL THAT USER ADDED!
+        return render(request, 'registration/registration_page.html')
+    else:
+        # TODO: SEND SIGNAL TO TEMPLATE TO INSTRUCT USER!
+        return render(request, 'registration/registration_page.html')
