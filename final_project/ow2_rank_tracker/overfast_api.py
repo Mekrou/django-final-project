8b2cdfbd78c3f6a77b9c377fdddcd7e3db4ec2ba
Mekrou <mills.ryan.011@gmail.com>
2023-12-09 20:25:18 -0500

    updating ranks from api to database works!

--- final_project/ow2_rank_tracker/overfast_api.py
+++ final_project/ow2_rank_tracker/overfast_api.py
@@ -1,22 +1,43 @@
 import requests
+from .models import Player
 
 api_base_url = "https://overfast-api.tekrop.fr/"
 
 # returns players rank in Players db model format for each role
 # EX: "grandmaster1", "bronze2", etc.
 def get_player_ranks(player_id):
-    # player_id is Name-Id format
-    # we should only have to supply this function either ID or Name, and via our db
-    # it should be able to find it.
     try:
-        response = requests.get(api_base_url + 'players/' + player_id + '/')
+        response = requests.get(api_base_url + 'players/' + player_id)
         if response.status_code == 200:
-            ranks = {'tank' : None, 'damage': None, 'support': None}
             data = response.json()
+            # This is mainly used for logging
+            nickname = data['summary']['username']
+
+            # Check if user profile is private
+            if (data['summary']['privacy'] == 'private'):
+                update_db_if_profile_private(data)
+                return None
+
+            # We'll store ranks in here
+            ranks = {'tank' : None, 'damage': None, 'support': None}
             ranks_data = data['summary']['competitive']['pc']
             for role in ranks:
-                ranks[role] = ranks_data[role]['division'] + (str) (ranks_data[role]['tier'])
+                try:
+                    ranks[role] = ranks_data[role]['division'] + (str) (ranks_data[role]['tier'])
+                except TypeError as e:
+                    print(f"{nickname} did not play {role} last season.")
+                    ranks[role] = 'unranked'
             return ranks
+        else:
+            print(f"{player_id} was {response.reason}")
     except requests.RequestException as e:
         print(f"Request error: {e}")
         return None
+
+def update_db_if_profile_private(data):
+    nickname = data['summary']['username']
+    print(f"{nickname} profile private. Cannot retrieve rank data!")
+    print(" Setting ranks to unknown in db..")
+    Player.objects.filter(nickname=nickname).update(tank_rank="unknown")
+    Player.objects.filter(nickname=nickname).update(damage_rank="unknown")
+    Player.objects.filter(nickname=nickname).update(support_rank="unknown")
--- final_project/ow2_rank_tracker/views.py
+++ final_project/ow2_rank_tracker/views.py
@@ -6,9 +6,21 @@ players = Player.objects.all()
 
 # Create your views here.
 def index(request):
+    populate_db_with_player_ranks()
     return render(request, 'ow2_rank_tracker/index.html')
 
 
 def populate_db_with_player_ranks():
     for player in players:
-        player.rank = get_player_ranks(player.player_id)
+        player_id = player.nickname + "-" + player.player_id
+        
+        player_ranks = get_player_ranks(player_id)
+        if (player_ranks):
+            for rank in player_ranks:
+                if (rank == 'tank'):
+                    Player.objects.filter(nickname=player.nickname).update(tank_rank=player_ranks[rank])
+                if (rank == 'damage'):
+                    Player.objects.filter(nickname=player.nickname).update(damage_rank=player_ranks[rank])
+                if (rank == 'support'):
+                    Player.objects.filter(nickname=player.nickname).update(support_rank=player_ranks[rank])
+                    
